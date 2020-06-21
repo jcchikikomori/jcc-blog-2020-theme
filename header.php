0a7555faa9b3077d236ca14e5a3cd43dfec76630
jccorsanes <jccorsanes@protonmail.com>
2020-06-21 20:20:21 +0800

    feat(bootstrap): initial setup

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php wp_title( '-', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
 	<?php wp_head(); ?>
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,1 +1,4 @@
+import 'jquery'
+import 'bootstrap'
+
 console.log('File `app.js` loaded')
